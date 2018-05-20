c9af36e9299dcfb90dfb84920696d56331ab2fb9
Claus Kühnel <info@ckuehnel.ch>
2018-05-20 10:31:36 +0200

    Add files via upload

---
+++ MKR1000_WakeUp/application.ino
@@ -0,0 +1,14 @@
+// This file contains the application code
+
+void run_application()
+{
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+
+    digitalWrite(LED_BUILTIN, HIGH);
+    delay(500);
+    digitalWrite(LED_BUILTIN, LOW);
+    delay(500);
+}
